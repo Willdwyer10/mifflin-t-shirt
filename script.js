ac81899c27e56a4709d71b350d1c98c751974e66
William Dwyer <william.dwyer@wisc.edu>
2025-03-29 17:46:35 -0500

    finished the site

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const orderButton = document.querySelector(".order-button");
+    orderButton.addEventListener("click", function() {
+        alert("Redirecting to the order page!");
+    });
+});
